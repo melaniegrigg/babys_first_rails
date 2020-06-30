3342d592dd4b5ffa9ad1dc2458229a41dc78a011
melaniegrigg <melanie.grigger@gmail.com>
2020-06-30 13:32:43 -0400

    make routes successful

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/welcomes" => "welcomes#hello"
   end
